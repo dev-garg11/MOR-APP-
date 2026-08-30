41821ce32eaef756f037d275c74aca46e23e3275
dev-garg11 <152768247+dev-garg11@users.noreply.github.com>
2026-08-30 22:14:14 +0530

    fix: resolve Platform ReferenceError in App.js fixing blank white screen

--- frontend/App.js
+++ frontend/App.js
@@ -2,7 +2,9 @@ import { StatusBar } from 'expo-status-bar';
 import React, { useEffect, useState } from 'react';
 import {
   ActivityIndicator,
+  Platform,
   SafeAreaView,
+  ScrollView,
   StyleSheet,
   Text,
   TouchableOpacity,
